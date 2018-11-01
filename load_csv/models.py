bfe2708afab76008964437b1621a9530cdb1d583
fariqodri <arfixfari@gmail.com>
2018-11-01 12:04:29 +0700

    Add neighbor model

--- load_csv/models.py
+++ load_csv/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Province(models.Model):
   name = models.CharField(max_length=20, unique=True)
   area = models.FloatField()
+
+class ProvinceNeighbor(models.Model):
+  province = models.ForeignKey(Province, on_delete=models.CASCADE)
+  neighbor = models.ForeignKey(Province, related_name="neighbor")
+
+  class Meta:
+    unique_together = (province, neighbor)
