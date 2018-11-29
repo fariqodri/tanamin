79dca025d6abf83ccfa9f4f830591668295e8a3a
fariqodri <arfixfari@gmail.com>
2018-11-29 13:12:48 +0700

    Lebih strict soal luas

--- csp/utils/localsearch.py
+++ csp/utils/localsearch.py
@@ -9,11 +9,8 @@ class LocalSearch:
 		self.csp = csp
 		self.big_to_small = big_to_small
 	
-	def check_solution(self, current):
-		for a,b in self.csp.constraints:
-			if current[a.name].color == current[b.name].color:
-				return False
-		return True
+	def check_solution(self, confs):
+		return (confs - 1) == 0
 
 	def conflicted_variable(self, current):
 		vars = []
@@ -31,27 +28,28 @@ class LocalSearch:
 		current[variable.name] = province
 		return self.calculate_conflicts(current)
 
-	def color_heuristic(self, variable, value, current):
-		current[variable.name].set_color(value)
+	def color_heuristic(self, current, variable=None, value=None, max_diff=200000):
+		if value:
+			current[variable.name].set_color(value)
 		max_color = self.find_biggest_color(current)
 		min_color = self.find_smallest_color(current)
-		if self.get_area_of_color(max_color, current) - self.get_area_of_color(min_color, current) >= 200000:
+		if self.get_area_of_color(max_color, current) - self.get_area_of_color(min_color, current) >= max_diff:
 			return False
 		return True
+	
+	def color_term(self, current, max_diff):
+		return self.color_heuristic(current, max_diff=max_diff)
 
 	def calculate_conflicts(self, current):
 		count = 0
 		for a,b in self.csp.graph.generate_edges():
 			if current[a.name].color == current[b.name].color:
-				# print(current[a.name], current[b.name])
 				count += 1
 		return count
 	
 	def assign(self):
-		# print("ASSIGN")
 		AREA = 8087393
 		area_divided = AREA // 4
-		self.max_area = area_divided
 		color_area = []
 		result = {}
 		for i in self.csp.domains:
@@ -63,46 +61,33 @@ class LocalSearch:
 			province = self.csp.variables[random.randint(0, len(self.csp.variables)-1)]
 			if not assigned_provinces.__contains__(province):
 				color_area.sort(key=lambda x: x[1], reverse=True)
-				# idx = random.randint(0, len(color_area) - 1)
 				color, area = color_area[0]
 				province.set_color(color)
 				color_area[0][1] -= province.area
 				result[province.name] = province
 				assigned_provinces.add(province)
 		result = dict(sorted(result.items(), key=lambda x : x[1].area, reverse=True))
-		# print(result)
 		return result
 
-	'''CARI VALUE YANG BISA MEMINIMALISIR KONFLIK DAN MEMPERTAHANKAN KONSISTENSI LUAS'''
 	def find_value(self, var, current):
 		confs = self.calculate_conflicts(current)
 		for value in self.csp.domains:
-			if self.conflicts(var, value, current) < confs and self.color_heuristic(var, value, current):
+			if self.conflicts(var, value, current) < confs and self.color_heuristic(current, var, value):
 					return value
 		smallest = self.find_smallest_color(current)
-		print("sml", smallest)
 		return smallest
 
 	def find_smallest_color(self, current):
 		r = self.get_color_data(current)
-		# for c in self.csp.domains:
-		# 	r[c] = self.get_area_of_color(c, current)
 		r = dict(sorted(r.items(), key=lambda x:x[1]))
 		return list(r.keys())[0]
 	
 	def find_biggest_color(self, current):
 		r = self.get_color_data(current)
-		# for c in self.csp.domains:
-		# 	r[c] = self.get_area_of_color(c, current)
 		r = dict(sorted(r.items(), key=lambda x:x[1], reverse=True))
 		return list(r.keys())[0]
 
 	def get_area_of_color(self, color, current):
-		# count = 0
-		# for i in current:
-		# 	if current[i].color == color:
-		# 		count += current[i].area
-		# return count
 		return self.get_color_data(current)[color]
 	
 	def get_color_data(self, current):
@@ -115,7 +100,7 @@ class LocalSearch:
 				res[v.color] = v.area
 		return res
 
-	def min_conflicts(self, max_steps=10000):
+	def min_conflicts(self, max_steps=1000):
 		current = self.assign()
 		prev_confs = self.calculate_conflicts(current)
 		same_times = 0
@@ -123,24 +108,23 @@ class LocalSearch:
 		while True:
 			for i in range(max_steps):
 				confs = self.calculate_conflicts(current)
-				if self.check_solution(current):
+				if self.check_solution(confs) and self.color_term(current, 300000):
+					print("GOOD FOUND")
 					return current
 				conflicts = self.conflicted_variable(current)
 				var = conflicts[random.randint(0, len(conflicts) - 1)]
 				value = self.find_value(var, current)
 				prev_confs = confs
-				print(same_times)
 				current[var.name].set_color(value)
 				if prev_confs <= confs:
 					same_times += 1
 				if prev_confs <= confs and same_times >= 1000:
 					break
 			count += 1
-			if prev_confs <= 3 or count > 50000:
+			if (prev_confs <= 3 and self.color_term(current, 300000)) or count > 10000:
+				print("BAD FOUND")
 				break
 			else:
 				current = self.assign()
 				prev_confs = self.calculate_conflicts(current)
-
-		print(self.get_color_data(current))
 		return current, self.conflicted_variable(current)
